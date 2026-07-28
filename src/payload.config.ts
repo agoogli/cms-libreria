640215329629909042cbae4b63f9a966befb0286
agoogli <agoogli@noname.com>
2026-07-28 13:14:42 +0200

    fix configurazione push bloccata in ambiente prod

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -46,7 +46,7 @@ export default buildConfig({
     pool: {
       connectionString: process.env.DATABASE_URL || '',
     },
-    push: process.env.NODE_ENV !== 'production',
+    push: process.env.PAYLOAD_DISABLE_PUSH !== 'true',
     prodMigrations:
       process.env.NODE_ENV === 'production' &&
       process.env.NEXT_PHASE !== 'phase-production-build'
